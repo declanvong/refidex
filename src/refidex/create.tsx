35e0df82fd3427a32035b0774cd94a409393e869
Declan Vong <declan@canva.com>
2020-07-02 11:44:32 +1000

    .

--- src/refidex/create.tsx
+++ src/refidex/create.tsx
@@ -2,11 +2,10 @@ import { observer } from 'mobx-react';
 import { RefidexModel } from 'model/model';
 import React from 'react';
 import { Refidex } from 'refidex/refidex';
-import { RefidexPresenter, RefidexStore } from 'refidex/refidex_presenter';
+import { RefidexStore } from 'refidex/refidex_presenter';
 
 export function createRefidex(model: RefidexModel) {
   const store = new RefidexStore(model);
-  const presenter = new RefidexPresenter();
 
   return observer(() => (
     <Refidex store={store}/>
